2ab4ac37aaee565ab640bb133ade9cee24b345bd
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-05-29 22:35:09 -0300

    Realizando scraping das 5 páginas necessárias

--- app/Http/Controllers/ScraperController.php
+++ app/Http/Controllers/ScraperController.php
@@ -2,9 +2,62 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+use Goutte\Client;
+use Carbon\Carbon;
+
+use App\Models\Tiding;
 
 class ScraperController extends Controller
 {
-    //
+    private $results = array();
+
+    public function index()
+    {
+        $tidings = Tiding::all();
+        return view('scraper.scraper', compact('tidings'));
+    }
+
+    public function scraper()
+    {
+        $itemsPerPage = 30;
+        $totalPages = 5;
+
+        $totalRecords = $itemsPerPage * $totalPages;
+
+        for($i = 0; $i < $totalRecords; $i = $i + $itemsPerPage) {
+            $client = new Client();
+            $url = "https://www.gov.br/compras/pt-br/acesso-a-informacao/noticias?b_start:int=$i";
+            $page = $client->request('GET', $url);
+    
+            $page->filter('.tileItem')->each(function($node) {
+                $linkNode = $node->filter('.url');
+                $dateNode = $node->filter('.summary-view-icon');
+    
+                $link = $linkNode->attr('href');
+                $title = $linkNode->text();
+    
+                $date = $dateNode->eq(0)->text();
+                $hours = Str::replace('h', ':', $dateNode->eq(1)->text());
+    
+                $timestamp = Carbon::createFromFormat('d/m/Y H:i', $date . ' ' . $hours)->toDateTimeString();
+                
+                $item = array(
+                    'title' => $title,
+                    'link' => $link,
+                    'timestamp' => $timestamp
+                );
+                
+                array_push($this->results, $item);
+            });
+        }
+
+        dd($this->results);
+
+        //$temp = array_unique(array_column($this->results, 'link'));
+        //$unique_arr = array_intersect_key($this->results, $temp);
+        //return view('scraper.scraper');
+        //dd($this->results, $unique_arr);
+    }
 }
