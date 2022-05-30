9e936edc64de33065d0a860b9e3cfd16db180507
Lucas Spagnolo Bombana <lucasspagnolobombana@gmail.com>
2022-05-30 18:19:35 -0300

    Removendo comentários desnecessários

--- app/Http/Controllers/TidingScraperController.php
+++ app/Http/Controllers/TidingScraperController.php
@@ -16,9 +16,5 @@ class TidingScraperController extends Controller
         
         Tiding::insert($newTidings);
         return redirect()->back();
-        //$temp = array_unique(array_column($this->results, 'link'));
-        //$unique_arr = array_intersect_key($this->results, $temp);
-        //return view('scraper.scraper');
-        //dd($this->results, $unique_arr);
     }
 }
